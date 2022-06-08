9b84aed2bbbae180626b4400af0575ef22601960
julianokoslowisk <47350166+julianokoslowisk@users.noreply.github.com>
2022-06-08 10:05:39 -0300

    Take BLIP TESTE

--- cypress/integration/Cenários/cenários.js
+++ cypress/integration/Cenários/cenários.js
@@ -1,25 +1,35 @@
 ///<reference types="Cypress" />
 
 
-  describe('Central de Atendimento ao Cliente TAT', function() {
+  describe('Blip Portal', function() {
     beforeEach(function(){
         cy.visit('https://account.blip.ai')
     })
 
-    it('Usuário Loga na Aplicação com dados Válidos', () => {
+    it('Cenário 1 : Usuário Loga na Aplicação com dados Válidos', () => {
       cy.get('#email').type('julianokoslowisk@gmail.com')
       cy.get('#password').type('Koslowisk123!@#')
       cy.get('#blip-login').click()
       cy.get('#logout-link').click()    
     })
 
-    it.only('Usuário Tenta logar na aplicação com dados Inválidos', () => {
+
+    
+      it('Cenário 4: Usuário esqueceu a senha ', () => {
+        cy.get('#login-forgot-password').click()
+        cy.get('.bp-fw-bold')
+        cy.get('#Email').type('julianokoslowisk@gmail.com')  
+        cy.get('#submitButton').click()
+        cy.get('.bp-c-rooftop')
+          
+      
+    })      
+
+     /*it('Usuário Tenta logar na aplicação com dados Inválidos', () => {
       cy.get('#email').type('julianokoslowisk@gmail.com')
       cy.get('#password').type('oslowisk123!@#')
       cy.get('#blip-login').click()
       cy.get('#logout-link').click()
-      .should('have.value','Login e/ou senha inválidos')
-     
-
-    })
+      cy.get('.bottom-left > .hydrated')
+        .should('have.value','Login e/ou senha inválidos')*/
   })
